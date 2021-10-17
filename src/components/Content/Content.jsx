8af81ff8036c2e3ef7f6dca3bb46729aafdba6b6
Andrii Svirskyi <andriisvirskyi@gmail.com>
2021-10-17 17:55:46 +0300

    learn ternary construction

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,42 +1,23 @@
 import { useState } from "react";
-import Button from './Button'
 
 import "./styles.scss";
 
 const Content = () => {
-  const [counter1, setCounter] = useState(0);
-  let counter = 0;
-  console.log("Ми зайшли в компоненту", counter);
+  const [counter, setCounter] = useState(0);
   return (
     <div className="content">
-      <div
-        className="button"
-        onClick={() => {
-          counter = counter + 1;
-          console.log(counter);
-          if(counter === 15){
-              alert('counter === 15')
-          }
-          //   setCounter(counter + 1);
-        }}
-      >
-        Click me
-      </div>
-
-      <div
-        className="button"
-        onClick={() => {
-          setCounter(counter1 + 1);
-          if(counter1 === 15){
-            alert('counter1 === 15')
-        }
-        }}
-      >
-        Click
-      </div>
-      <Button />
+        <div
+          className={`button ${counter >= 10 ? 'disabled' : ''}`}
+          onClick={() => {
+            if(counter < 10){
+              setCounter(counter + 1);
+            }
+          }}
+        >
+          Click
+        </div>
       <div className="counter">{counter}</div>
-      <div className="counter">{counter1}</div>
+      {counter >= 10 ? <div className="element">Element</div> : null}
     </div>
   );
 };
