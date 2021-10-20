e959f49a81a9c1e59f984d1a214c6b52e6b391ca
Andrii Svirskyi <andriisvirskyi@gmail.com>
2021-10-20 21:20:36 +0300

    add checkout

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,27 +1,37 @@
 import { useState } from "react";
 import "./styles.scss";
-import Form from '../Form/Form'
+import Form from "../Form/Form";
 
 const Content = () => {
   const [counter, setCounter] = useState(0);
+  const [isForm, setForm] = useState(false);
+
   return (
     <div className="content">
-        <div
-          className={`button ${counter >= 10 ? 'disabled' : ''}`}
-          onClick={() => {
-            if(counter < 10){
-              setCounter(counter + 1);
-            }
-          }}
-        >
-          Click
-        </div>
+      <div
+        className={`button ${counter >= 10 ? "disabled" : ""}`}
+        onClick={() => {
+          if (counter < 10) {
+            setCounter(counter + 1);
+          }
+        }}
+      >
+        Click
+      </div>
       <div className="counter">{counter}</div>
       {counter >= 10 ? <div className="element">Element</div> : null}
-      <Form />
+      <div
+        className="form-checkbox"
+        onClick={() => {
+          setForm(!isForm);
+        }}
+      >
+        <div>Show form</div>
+        <input type="checkbox" checked={isForm} />
+      </div>
+      {isForm ? <Form /> : null}
     </div>
   );
 };
- 
 
 export default Content;
